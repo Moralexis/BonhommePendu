9f2f2c3eb9818accfe1cae05d7552511344c28bf
Alexis Morataya <2091848@cegepmontpetit.ca>
2026-09-22 10:33:03 -0400

    a

--- BonhommePendu/Events/GuessedLetterEvent.cs
+++ BonhommePendu/Events/GuessedLetterEvent.cs
@@ -7,14 +7,14 @@ namespace BonhommePendu.Events
     public class GuessedLetterEvent : GameEvent
     {
         public override string EventType { get { return "GuessedLetter"; } }
-        //public char Letter { get; set; }
+        public char Letter { get; set; }
         // TODO: Compléter
         public GuessedLetterEvent(GameData gameData, char letter)
         {
             if (!gameData.GuessedLetters.Contains(letter))
             {
                 gameData.GuessedLetters.Add(letter);
-                //Letter = letter;
+                Letter = letter;
             }
         }
         
